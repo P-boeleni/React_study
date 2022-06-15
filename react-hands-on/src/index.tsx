452c37d5ae45a795c50a9fc4bad48470c2476392
P-boeleni <ita.ark.takahiroh@gmail.com>
2022-06-15 10:14:15 +0900

    React初期設定

--- react-hands-on/src/index.tsx
+++ react-hands-on/src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import App from "./components/color/App";
+import ColorProvider from "./components/color/ColorProvider";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+// import data from "../data/recipes.json";
+// import Menu from "./components/recipe/Menu";
+// import StarRating from "./components/starRating/StarRating";
+// import App from "./components/starRating/App";
+// import AddColorForm from "./components/form/AddColorFrom";
+
+// render(<Menu recipes={data}/>, document.getElementById("root"));
+// render(<StarRating/>, document.getElementById("star-rating"));
+// render(<App/>, document.getElementById("star-rating"));
+// render(<AddColorForm />, document.getElementById("form"));
+// render(<App />, document.getElementById("color"));
+
+render(
+  // Providerで囲むことで、一部のコンポーネントにデータを公開することができる
+  // この場合、Appに含まれる以下コンポーネントは、colorsを受け取ることができる
+  <ColorProvider>
     <App />
-  </React.StrictMode>
+  </ColorProvider>,
+  document.getElementById("color")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
